299ea4573c06c2f5da97e27c5544f740dc2d47c9
HashiniJayasinghe <hashiniheshadi@gmail.com>
2023-10-03 10:17:21 +0530

    Fix for sever.js

--- server.js
+++ server.js
@@ -1,45 +1,42 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
 
-require("dotenv").config();
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(helmet()); // Apply Helmet middleware
+app.use(cookieParser());
 
 const port = process.env.PORT || 5000;
 const uri = process.env.MONGO_URI;
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connection.once("open", () => {
-  console.log("MongoDB Connected");
+mongoose.connection.once('open', () => {
+  console.log('MongoDB Connected');
 });
 
-app.get("/", (req, res) => {
-  res.send("Api running");
+app.get('/', (req, res) => {
+  res.send('API running');
 });
 
-app.use("/api/admins", require("./routes/Admin.route"));
-app.use("/api/notices", require("./routes/Notice.route"));
-
-app.use("/api/students", require("./routes/Student.route"));
-app.use("/api/studentGroups", require("./routes/StudentGroup.route"));
-app.use("/api/studentProjects", require("./routes/StudentProject.route"));
-
-app.use("/api/staffMembers", require("./routes/Staff.route"));
-
-
-
-
-
-app.use("/api/hostaldetails", require("./routes/HostalService.route"));
-app.use("/api/transportdetails", require("./routes/TransportService.route"));
-
-app.use("/api/quiz", require("./routes/Quiz.route"));
-app.use("/api/marking", require("./routes/Marking.routes"));
-
+// Define your routes here...
+app.use('/api/admins', require('./routes/Admin.route'));
+app.use('/api/notices', require('./routes/Notice.route'));
+app.use('/api/students', require('./routes/Student.route'));
+app.use('/api/studentGroups', require('./routes/StudentGroup.route'));
+app.use('/api/studentProjects', require('./routes/StudentProject.route'));
+app.use('/api/staffMembers', require('./routes/Staff.route'));
+app.use('/api/hostaldetails', require('./routes/HostalService.route'));
+app.use('/api/transportdetails', require('./routes/TransportService.route'));
+app.use('/api/quiz', require('./routes/Quiz.route'));
+app.use('/api/marking', require('./routes/Marking.routes'));
 
 app.listen(port, () => {
-  console.log("Server is starting on port " + port);
+  console.log(`Server is starting on port ${port}`);
 });
